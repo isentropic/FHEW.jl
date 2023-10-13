2baebbd24af02fa463137d3280e7c815fc91134e
Zhanibek <zhanibek.om@gmail.com>
2023-10-13 14:54:39 +0900

    add benchmarking

--- test/runtests.jl
+++ test/runtests.jl
@@ -18,18 +18,3 @@ using Test, BenchmarkTools
         end
     end
 end
-
-@testset "benchmarking" begin
-    c = generate_context()
-    sk, evk = generate_keys(c)
-
-
-    m0 = 0
-    m1 = 1
-
-    ct0 = encryptLWE(m0, c.n, c.q, sk, c.stddev)
-    ct1 = encryptLWE(m1, c.n, c.q, sk, c.stddev)
-
-    @btime nand_bootstrapping(ct0, ct1, evk, c)
-    @benchmark nand_bootstrapping(ct0, ct1, evk, c)
-end
